86bce9651b7cd1f0b1b7634eca2d5a19bee67f6b
spaceman860 <spaceman8583@gmail.com>
2015-03-06 01:21:33 +0000

    Revert "(2/2) Themes: Fix SystemUI failing to get themed"
    This reverts commit f2b2dab64d9f753c0fdade1fe0ab6b49583fc70f.
    
    Change-Id: Iac3a587517865ff54faa8654fa57edb67ae1cb53

--- src/org/cyanogenmod/themes/provider/ThemePackageHelper.java
+++ src/org/cyanogenmod/themes/provider/ThemePackageHelper.java
@@ -40,6 +40,7 @@ import java.util.List;
 import java.util.Map;
 
 import static android.content.res.ThemeConfig.SYSTEMUI_NAVBAR_PKG;
+import static android.content.res.ThemeConfig.SYSTEMUI_STATUS_BAR_PKG;
 import static android.content.res.ThemeConfig.SYSTEM_DEFAULT;
 
 /**
@@ -312,6 +313,9 @@ public class ThemePackageHelper {
         }
         if (pkgName.equals(themeConfig.getOverlayPkgName())) {
             reApply.add(ThemesColumns.MODIFIES_OVERLAYS);
+        }
+        if (pkgName.equals(themeConfig.getOverlayPkgNameForApp(SYSTEMUI_STATUS_BAR_PKG))) {
+            reApply.add(ThemesColumns.MODIFIES_STATUS_BAR);
         }
         if (pkgName.equals(themeConfig.getOverlayPkgNameForApp(SYSTEMUI_NAVBAR_PKG))) {
             reApply.add(ThemesColumns.MODIFIES_NAVIGATION_BAR);
